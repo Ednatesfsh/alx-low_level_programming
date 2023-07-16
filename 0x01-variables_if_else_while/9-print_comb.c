091e5f6ee3aabd14750acfca23f8d04c2d80f9ac
Ednatesfsh <editesfsh.22@gmail.com>
2023-07-16 16:27:42 -0700

    9-print_comb | cat -e

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,16 +3,16 @@
 #include <time.h>
 /**
  *main - Prints all possible combinations of single-digit numbers
- * Return: Always (Success)
+ *Return: Always (Success)
  */
 int main(void)
 {
 int y;
-for (y = 0; y <= 9; y++)
+for (y = '0'; y <= '9'; y++)
 {
 putchar((y % 10) + '0');
-if
-(y == 9);
+}
+{
 putchar(',');
 putchar(' ');
 }
