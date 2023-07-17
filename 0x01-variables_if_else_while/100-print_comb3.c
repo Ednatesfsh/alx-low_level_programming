12a30f8991bfcdc249ab0e1a65f00cb298257128
Ednatesfsh <editesfsh.22@gmail.com>
2023-07-17 03:24:56 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,14 @@ for (d1 = '0'; d1 < '9'; d1++)
 {
 for (d2 = d1 + 1; d2 <= '9'; d2++)
 {
+if (d2 != d1)
+{
 putchar(d1);
 putchar(d2);
 if (d1 == '8' && d2 == '9')
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
