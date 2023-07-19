c004990c2de43179481c7ffec6afbdd8165d0303
Ednatesfsh <editesfsh.22@gmail.com>
2023-07-18 20:55:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+  2 /**
+  3   * main - Entry point
+  4   * Description: 'prints _putchar to stdout'
+  5   * Return: always 0
+  6   */
+  7 int main(void)
+  8 {
+  9          _putcha('_');
+ 10          _putcha('p');
+ 11          _putcha('u');
+ 12          _putcha('t');
+ 13          _putcha('c');
+ 14          _putcha('h');
+ 15          _putcha('a');
+ 16          _putcha('r');
+ 17          _putcha('\n');
+ 18          return (0);
+ 19 }
