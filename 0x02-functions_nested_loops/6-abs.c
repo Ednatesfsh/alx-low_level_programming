2e4dccaaf3f26ec50d4dfe974ae7711e8a16726a
Ednatesfsh <editesfsh.22@gmail.com>
2023-07-19 20:31:28 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - calculate the absolute value of an integer
+ * @i: the integer to be converted
+ * Return: absolute value of the input number
+ */
+int _abs(int i)
+{
+if (i <= 0)
+{
+	return (i);
+}
+else
+{
+	return (i * -1);
+}
+}
