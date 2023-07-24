d2f64096d74e76dde3e6cb5a7e3eea28b7d0b432
Ednatesfsh <editesfsh.22@gmail.com>
2023-07-24 14:01:03 -0700

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,13 @@
 /**
   * _puts - prints a string,  followed by a new line, to stdou
   * @str: string to print
+  * Return: void
   */
 void _puts(char *str)
 {
-while (*str != '\0')
+while (;*str != '\0'; str++)
 {
-	_putchar(*str++);
+	_putchar(*str);
 }
 	_putchar('\n');
 }
