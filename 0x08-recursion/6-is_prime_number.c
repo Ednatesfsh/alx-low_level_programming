8e60b82952003e7f2024daad2f03ccea868f4ea6
Ednatesfsh <editesfsh.22@gmail.com>
2023-08-03 02:02:19 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -22,7 +22,7 @@ int check_prime(int n, int y)
 {
 	if (y >= n && n > 1)
 		return (1);
-	else if (n % y == || n <= 1)
+	else if (n % y == 0 || n <= 1)
 		return (0);
 	else
 		return (check_prime(n, y + 1));
