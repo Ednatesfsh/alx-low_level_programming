4ef969bd439556b10df199e51914a61c7cfc9c23
Ednatesfsh <editesfsh.22@gmail.com>
2023-08-04 14:40:59 -0700

    updated

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: input number as an integer
+ * Return: absolute value of the input number
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+	return (i);
+}
+else
+{
+	return (i * -1);
+}
+}
