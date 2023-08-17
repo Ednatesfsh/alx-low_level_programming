c758da1328c169e097b2b56731c2100466bddf98
Ednatesfsh <editesfsh.22@gmail.com>
2023-08-17 12:20:51 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -7,10 +7,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(intargc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int bytes, i;
-	char arr;
+	char *arr;
 
 	if (argc != 2)
 	{
@@ -18,7 +18,7 @@ int main(intargc, char *argv[])
 		exit(1);
 	}
 	bytes = atoi(argv[1]);
-	if (bytes <)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
@@ -28,10 +28,10 @@ int main(intargc, char *argv[])
 	{
 		if (i == bytes - 1)
 		{
-			printf("%02hhx\n" arr[i]);
+			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("02hhx ", arr[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
