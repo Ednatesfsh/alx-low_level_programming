367771c94e2e2ff1b6b98b84b8b7c61edfca6bd1
Ednatesfsh <editesfsh.22@gmail.com>
2023-08-26 05:20:29 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,19 +11,15 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i ; i++)
 		{
-			_putchar(32);
+		for (j = 0; j <= i; j++)
+			_putchar(' ');
 		}
-		_putchar(92);
-		_putchar('\n');
-	}
+			_putchar('\\');
+			_putchar('\n');
 	}
 }
